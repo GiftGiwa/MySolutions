4a57e39617ad0a79ee9250d336bef62f5ccf442b
GiftGiwa <gdg2131@columbia.edu>
2021-12-08 23:51:10 -0500

    Create Main.java

---
+++ etothenthnumber/Main.java
@@ -0,0 +1,9 @@
+package etothenthnumber;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(eNthNumber(5));
+    }
+    public static double eNthNumber(int n) {
+        return 2 + (int) ((Math.exp(1) - 2) / Math.pow(10, -1 * n)) / (Math.pow(10, n));
+    }
+}

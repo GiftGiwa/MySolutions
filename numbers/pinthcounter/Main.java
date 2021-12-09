6d2982ec3b1f434468026e62a183a52293372cec
GiftGiwa <gdg2131@columbia.edu>
2021-12-09 18:54:25 -0500

    Update Main.java

--- numbers/pinthcounter/Main.java
+++ numbers/pinthcounter/Main.java
@@ -1,9 +1,10 @@
 package numbers.pinthcounter;
 public class Main {
     public static void main(String[] args) {
-        System.out.println(piNthNumber(2)); 
+        System.out.println(piNthNumber(4)); 
     }
     public static double piNthNumber(int n) {
-        return 3 + (int) ((Math.PI - 3 / Math.pow(10, -1 * n))) / (Math.pow(10, n));
+        int wholeNumber = (int) ( (Math.PI - 3.0) / Math.pow(10, -1 * n));
+        return 3 + (wholeNumber / Math.pow(10, n));
     }
 }
